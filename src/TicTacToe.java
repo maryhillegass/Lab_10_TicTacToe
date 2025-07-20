c1baf39fda874461476d50bbd279b05281138e5e
maryhillegass <maryhillegass@gmail.com>
2025-07-20 16:41:19 -0400

    Clear and Display Board

---
+++ src/TicTacToe.java
@@ -0,0 +1,45 @@
+public class TicTacToe {
+    private static final int ROWS = 3;
+    public static final int COLS = 3;
+    private static String board [][] = new String[ROWS][COLS];
+
+    public static void main(String[] args) {
+        clearBoard();
+        display();
+        board[1][1] = "O";
+        board[0][0] = "x";
+        display();
+        clearBoard();
+        display();
+    }
+
+    /*
+    Clears the TicTacToe Board
+     */
+    private static void clearBoard(){
+        //Enters " " into all spaces on the board
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                board[i][j] = " ";
+            }
+        }
+    }
+
+    /*
+    Displays the TicTacToe Board
+     */
+    private static void display(){
+        System.out.printf("\n");
+        for (int i = 0; i < ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                System.out.printf(" %s ", board[i][j]);
+                if (j < 2)
+                    System.out.printf("|");
+            }
+            if (i < 2)
+                System.out.printf("\n------------");
+            System.out.println("");
+        }
+    }
+
+}
