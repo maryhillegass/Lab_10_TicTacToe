722d3b3bbcdb798e552c257e4aee329cb0de41cf
maryhillegass <maryhillegass@gmail.com>
2025-07-20 16:57:43 -0400

    isColWin complete

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,9 +7,13 @@ public class TicTacToe {
         clearBoard();
         display();
         board[1][1] = "O";
-        board[0][0] = "x";
-        System.out.println("Valid move 11 should be false: " + isValidMove(1,1));
-        System.out.println("Valid move 10 should be true: " + isValidMove(1,0));
+        board[0][0] = "X";
+        board[1][0] = "X";
+        board[2][0] = "X";
+        board[0][1] = "O";
+        board[2][1] = "O";
+        System.out.println("Column 1 should be a win for O: " + isColWin("O"));
+        System.out.println("Column 0 should be a win for X: " + isColWin("X"));
         display();
         clearBoard();
         display();
@@ -49,4 +53,16 @@ public class TicTacToe {
         return validMove;
     }
 
+    private static boolean isWin(String player){
+        return false;
+    }
+
+    private static boolean isColWin(String player){
+        for (int i = 0; i < COLS; i++) {
+            if (board[0][i].equals(player) && board[1][i].equals(player) && board[2][i].equals(player)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
